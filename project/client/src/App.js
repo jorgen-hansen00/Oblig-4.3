d32fa1731cc4af79691683c2a6baff481e5ff513
jorge <jorgen.hansen00@gmail.com>
2021-03-24 19:46:53 +0100

    final

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -1,5 +1,7 @@
 import Routes from './routes/Routes';
 import Theme from './styles/Theme';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 /**
  * Importerer Routes her da den "styrer" hele applikasjonen
@@ -13,5 +15,28 @@ function App() {
     </Theme>
   );
 }
+
+function Movies() {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('The button was clicked.');
+  }
+
+  return (
+    <a href="#" onClick={handleClick}>
+      Click me
+    </a>
+  );
+  ReactDOM.render(
+    <Movies />,
+    document.getElementById('root')
+  );
+}
+
+function Movie(props) {
+  return <h1>Movies</h1>;
+}
+
+export default App;
 
 export default App;
